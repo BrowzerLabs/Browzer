fe1c3edede79651be8fb38ca12b8c4666fdc7e44
Aditya <aditya@trybrowzer.com>
2025-11-10 12:33:36 +0530

    refactor: remove ClaudeClient.

--- src/main/llm/index.ts
+++ src/main/llm/index.ts
@@ -2,7 +2,6 @@ export * from './core';
 export * from './builders';
 export * from './utils';
 
-export { ClaudeClient } from './clients/ClaudeClient';
 export { AutomationClient } from './clients/AutomationClient';
 export { AutomationPlanParser } from './parsers/AutomationPlanParser';
-export { AutomationService, IterativeAutomationResult } from './AutomationService';
+export { AutomationService } from './AutomationService';
