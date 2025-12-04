672117419807cbc018b4ee0d15ec3771e03752a4
abhi-browzer <abhinandan@trybrowzer.com>
2025-12-04 22:53:48 +0530

    refactor: remove automation settings and homepage field from settings schema
    - Remove automation settings category (defaultProvider, geminiApiKey, claudeApiKey, openaiApiKey) from AppSettings interface and defaultSettings
    - Remove homepage field from general settings
    - Remove settings:automation event from SettingsServiceEvents interface
    - Remove off method from SettingsService class
    - Remove onSettingsChanged event listener from BrowserAPI
    - Remove inline comments from AppSettings interface

--- src/preload/types/browser.types.ts
+++ src/preload/types/browser.types.ts
@@ -126,9 +126,6 @@ export interface BrowserAPI {
   // Deep Link event listeners
   onDeepLink: (callback: (path: string) => void) => () => void;
   
-  // Settings event listeners
-  onSettingsChanged: (callback: (data: { category: string; value: any }) => void) => () => void;
-  
   // Deep Link actions
   hideAllTabs: () => Promise<boolean>;
   showAllTabs: () => Promise<boolean>;
